ec0a24efc90f7b667c6ffc6d082097873cfeab78
ibb2 <70052141+ibb2@users.noreply.github.com>
2026-08-09 15:50:37 +1000

    Fetching the article body working.

--- entrypoints/page-reader.content/index.tsx
+++ entrypoints/page-reader.content/index.tsx
@@ -24,8 +24,37 @@ export default defineContentScript({
     // Executed when content script is loaded, can be async
 
     const content = document.body.innerHTML;
-    const article = document.body.querySelector("article")?.innerText
+    const rawArticle = document.body.querySelector("article");
+    console.log("Article html", document.body.querySelector("article"));
+    const article = parseHtml(rawArticle!);
+    // const article = document.body.querySelector("article")?.innerText
 
     browser.runtime.sendMessage(article);
   },
 });
+
+const parseHtml = (html: HTMLElement) => {
+  const h1 = html.querySelector("h1");
+
+  console.log("h1", h1);
+  const paragraphs = p(html);
+  console.log("paragraphs", paragraphs);
+
+  const body = [h1?.innerText, ...paragraphs];
+
+  return body;
+};
+
+const p = (html: HTMLElement) => {
+  const paragraphs: string[] = [];
+
+  const ps = html.querySelectorAll("p");
+
+  console.log("pargraphs", ps);
+
+  ps.forEach((p) => {
+    paragraphs.push(p.innerText);
+  });
+
+  return paragraphs;
+};
--- entrypoints/sidepanel/App.tsx
+++ entrypoints/sidepanel/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [pageContent, setPageContent] = useState("");
+  const [pageContent, setPageContent] = useState<Array<string>>([""]);
 
   browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log(message);
@@ -12,21 +12,12 @@ function App() {
 
   return (
     <main className="min-h-screen space-y-4 bg-background p-4 text-foreground">
-      <div>
-        <h1 className="text-xl font-semibold">Levy</h1>
-        <p className="text-sm text-muted-foreground">
-          Your React and shadcn/ui side panel is ready.
-        </p>
-      </div>
-
       <section className="flex flex-col items-start gap-3 rounded-lg border bg-card p-4 shadow-sm">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
-        </Button>
-        <p className="text-sm text-muted-foreground">
-          Edit <code>entrypoints/sidepanel/App.tsx</code> to get started.
+        <p>
+          {pageContent.map((item) => (
+            <div>{item}</div>
+          ))}
         </p>
-        <p>{pageContent}</p>
       </section>
     </main>
   );
