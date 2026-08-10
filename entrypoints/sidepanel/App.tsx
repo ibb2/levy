182e9722828784b6a5753374210412f7defc04dd
ibb2 <70052141+ibb2@users.noreply.github.com>
2026-08-10 14:34:30 +1000

    Added a no articles message

--- entrypoints/sidepanel/App.tsx
+++ entrypoints/sidepanel/App.tsx
@@ -13,19 +13,18 @@ function App() {
   });
 
   return (
-    <main className="min-h-screen space-y-4 bg-background p-4 text-foreground">
-      <section className="flex flex-col items-start gap-3 rounded-lg bg-card p-4">
-        {pageContent !== null && pageContent !== undefined && (
-          <p>
-            {pageContent.map((node: Element) => {
-              const headingStyles: Record<number, string> = {
-                1: "text-4xl font-bold leading-tight",
-                2: "text-3xl font-semibold leading-tight",
-                3: "text-2xl font-semibold leading-snug",
-                4: "text-xl font-semibold leading-snug",
-                5: "text-lg font-medium leading-snug",
-                6: "text-base font-medium leading-normal",
-              };
+    <main className="flex flex-col min-h-screen space-y-4 bg-background p-4 text-foreground">
+      {pageContent !== null && pageContent !== undefined ? (
+        <p>
+          {pageContent.map((node: Element) => {
+            const headingStyles: Record<number, string> = {
+              1: "text-4xl font-bold leading-tight",
+              2: "text-3xl font-semibold leading-tight",
+              3: "text-2xl font-semibold leading-snug",
+              4: "text-xl font-semibold leading-snug",
+              5: "text-lg font-medium leading-snug",
+              6: "text-base font-medium leading-normal",
+            };
 
               const nodeHierarchyStyling =
                 node.type === "paragraph"
@@ -34,15 +33,18 @@ function App() {
                     ? (headingStyles[node.level] ?? headingStyles[6])
                     : "";
 
-              return (
-                <div className={cn("py-1", nodeHierarchyStyling)}>
-                  {node.text}
-                </div>
-              );
-            })}
-          </p>
-        )}
-      </section>
+            return (
+              <div className={cn("py-1", nodeHierarchyStyling)}>
+                {node.text}
+              </div>
+            );
+          })}
+        </p>
+      ) : (
+        <div className="m-auto">
+          <p>Sorry, I couldn't find any articles 😔</p>
+        </div>
+      )}
     </main>
   );
 }
